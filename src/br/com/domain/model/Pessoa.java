3ad3e86c0972a4a0b1f4bd4d4abe29ef38912e70
Miriam Maranhão Da Silva <98363007+im-maranhao@users.noreply.github.com>
2026-01-14 18:04:27 -0300

    adicionando classes abstratas

---
+++ src/br/com/domain/model/Pessoa.java
@@ -0,0 +1,14 @@
+package br.com.domain.model;
+
+@MappedSuperclass
+public abstract class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    protected String nome;
+    protected String email;
+
+    public abstract String getTipo();
+}
