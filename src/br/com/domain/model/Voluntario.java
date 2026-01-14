b33087885baee2c07ab80e46705613455e7e5a4b
Miriam Maranhão Da Silva <98363007+im-maranhao@users.noreply.github.com>
2026-01-14 18:12:40 -0300

    adicionando classes abstratas

---
+++ src/br/com/domain/model/Voluntario.java
@@ -0,0 +1,13 @@
+package br.com.domain.model;
+
+@Entity
+public class Voluntario extends Pessoa {
+
+    private String telefone;
+    private Boolean ativo;
+
+    @Override
+    public String getTipo() {
+        return "VOLUNTARIO";
+    }
+}
