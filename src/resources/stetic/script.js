90ccbed92e85b2f9a00f19f745a043af1f7c11b7
Miriam Maranhão Da Silva <98363007+im-maranhao@users.noreply.github.com>
2026-01-22 23:16:07 -0300

    refactor front and consumo da api

---
+++ src/resources/stetic/script.js
@@ -0,0 +1,35 @@
+const API = "http://localhost:8080/voluntarios";
+
+function cadastrar() {
+  fetch(API, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      nome: nome.value,
+      email: email.value,
+      telefone: telefone.value
+    })
+  }).then(() => listar());
+}
+
+function listar() {
+  fetch(API)
+    .then(r => r.json())
+    .then(dados => {
+      lista.innerHTML = "";
+      dados.forEach(v => {
+        lista.innerHTML += `
+          <li>
+            ${v.nome} - ${v.email}
+            <button onclick="deletar(${v.id})">X</button>
+          </li>`;
+      });
+    });
+}
+
+function deletar(id) {
+  fetch(`${API}/${id}`, { method: "DELETE" })
+    .then(() => listar());
+}
+
+listar();
