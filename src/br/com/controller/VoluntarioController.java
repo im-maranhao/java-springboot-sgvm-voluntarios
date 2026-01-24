7013a36e19b4153e5d00d9cc1dfc5aa4ecaa59e6
Miriam Maranhão Da Silva <98363007+im-maranhao@users.noreply.github.com>
2026-01-24 02:58:30 -0300

    functions  voluntario controller

--- src/br/com/controller/VoluntarioController.java
+++ src/br/com/controller/VoluntarioController.java
@@ -1,5 +1,7 @@
 package br.com.controller;
 
+import br.com.domain.model.Voluntario;
+
 @RestController
 @RequestMapping("/voluntarios")
 public class VoluntarioController {
@@ -9,4 +11,20 @@ public class VoluntarioController {
     public VoluntarioController(VoluntarioService service){
         this.service = service;
     }
+
+    @PostMapping
+    public Voluntario cadastrar(@RequestBody Voluntario voluntario){
+        return service.cadastrar(voluntario);
+    }
+
+    @GetMapping
+    public List<Voluntario> listar(){
+        return service.listar();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id){
+        service.deletar(id);
+    }
+
 }
