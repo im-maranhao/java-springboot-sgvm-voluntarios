e275f55c0ac3af084cbdc45f19d9f335f90b94c3
Miriam Maranhão Da Silva <98363007+im-maranhao@users.noreply.github.com>
2026-01-27 00:09:39 -0300

    atualizando service

--- src/br/com/service/VoluntarioService.java
+++ src/br/com/service/VoluntarioService.java
@@ -1,24 +1,40 @@
 package br.com.service;
 
+import br.com.domain.model.Voluntario;
 import br.com.repository.VoluntarioRepository;
+import br.com.service.contracts.CadastroService;
+import org.springframework.stereotype.Service;
 
-public class VoluntarioService {
+import java.util.List;
+
+@Service
+public class VoluntarioService implements CadastroService<Voluntario> {
+
     private final VoluntarioRepository repository;
 
-    public VoluntarioService(VoluntarioRepository repository){
+    public VoluntarioService(VoluntarioRepository repository) {
         this.repository = repository;
     }
 
-
-    public Voluntario cadastrar(Voluntario voluntario) {
+    @Override
+    public Voluntario salvar(Voluntario voluntario) {
         voluntario.setAtivo(true);
         return repository.save(voluntario);
     }
 
-    public List<Voluntario> listar() {
-        return repository.findAll();
+    @Override
+    public Voluntario buscarPorId(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Voluntário não encontrado"));
     }
+
+    @Override
     public void deletar(Long id) {
         repository.deleteById(id);
+    }
+
+    // método EXTRA (não faz parte do contrato)
+    public List<Voluntario> listar() {
+        return repository.findAll();
     }
 }
