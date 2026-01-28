3c3e3f1a0e566043ac4163fa42d43600ffc6350c
Miriam Maranhão Da Silva <98363007+im-maranhao@users.noreply.github.com>
2026-01-28 00:19:25 -0300

    NotBlank email

--- src/br/com/domain/dto/VoluntarioDTO.java
+++ src/br/com/domain/dto/VoluntarioDTO.java
@@ -6,4 +6,7 @@ public class VoluntarioDTO {
     @NotBlank(message = "Nome é obrigatório")
     private String nome;
 
+    @Email(message = "Email inválido")
+    @NotBlank
+    private String email;
 }
