8cc4d6ce03b553706508e8e45b86fce7f5c36060
Miriam Maranhão Da Silva <98363007+im-maranhao@users.noreply.github.com>
2026-01-27 23:57:40 -0300

    atl service voluts

--- src/br/com/service/VoluntarioService.java
+++ src/br/com/service/VoluntarioService.java
@@ -12,29 +12,22 @@ public class VoluntarioService implements CadastroService<Voluntario> {
 
     private final VoluntarioRepository repository;
 
+    private final VoluntarioRepository repository;
+
     public VoluntarioService(VoluntarioRepository repository) {
         this.repository = repository;
     }
 
-    @Override
-    public Voluntario salvar(Voluntario voluntario) {
+    public Voluntario cadastrar(Voluntario voluntario) {
         voluntario.setAtivo(true);
         return repository.save(voluntario);
     }
 
-    @Override
-    public Voluntario buscarPorId(Long id) {
-        return repository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Voluntário não encontrado"));
-    }
-
-    @Override
-    public void deletar(Long id) {
-        repository.deleteById(id);
-    }
-
-    // método EXTRA (não faz parte do contrato)
     public List<Voluntario> listar() {
         return repository.findAll();
+    }
+
+    public void deletar(Long id) {
+        repository.deleteById(id);
     }
 }
